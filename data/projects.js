ed6f67fd91302449bf04db20aaea7fd574760780
Korchat123 <korchat123@gmail.com>
2026-03-31 16:00:26 +0700

    change excersice2

--- data/projects.js
+++ data/projects.js
@@ -12,6 +12,13 @@ const projects = [
   // ADD YOUR ENTRY HERE - copy the object below as your template
 
   {
+    member: "Team member1",
+    project: "DevTeam Hub",
+    description: "The team portfolio site you are looking at right now.",
+    status: "progess",
+    github: "#",
+  },
+  {
     member: "Team Lead",
     project: "DevTeam Hub",
     description: "The team portfolio site you are looking at right now.",
